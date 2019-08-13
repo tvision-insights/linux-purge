a6bd21319ccb53adf0b9499e9b602dc6c797324a
Jarno Ilari Suni <8@iki.fi>
2019-08-13 18:48:48 +0300

    Added script for removing installation of linux-purge

---
+++ remove-linux-purge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+set -o nounset
+set -o errexit
+
+# Ensure superuser privileges, if required
+[[ $EUID -ne 0 ]] && {
+	printf '%s\n' 'Must be run as superuser' >&2
+	exit 1
+}
+
+rm -vf /usr/local/bin/linux-purge
+
+rm -vf /etc/bash_completion.d/linux-purge
+rm -vf /usr/share/bash-completion/completions/linux-purge
+rm -vf /usr/local/share/bash-completion/completions/linux-purge
+rm -vf /usr/local/share/bash-completion/completions/linux-purge.bash
+
+rm -vf /usr/local/man/man8/linux-purge.8
+rm -vf /usr/local/man/man8/linux-purge.8.gz
+mandb -q
